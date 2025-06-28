83ea4481e356390c9fcfba665acae7de0fb3bac8
Linho <lhp@linho.cc>
2025-06-28 23:37:48 +0800

    执行全量格式化

--- .vitepress/locales/index.ts
+++ .vitepress/locales/index.ts
@@ -4,24 +4,24 @@ import zh_TW from './zh_TW'
 import en from './en'
 
 export default defineConfig({
-    locales: {
-        root: {
-            label: '简体中文',
-            lang: zh_CN.lang,
-            themeConfig: zh_CN.themeConfig,
-            description: zh_CN.description
-        },
-        zh_TW: {
-            label: '繁体中文',
-            lang: zh_TW.lang,
-            themeConfig: zh_TW.themeConfig,
-            description: zh_TW.description
-        },
-        en: {
-            label: 'English',
-            lang: en.lang,
-            themeConfig: en.themeConfig,
-            description: en.description
-        }
+  locales: {
+    root: {
+      label: '简体中文',
+      lang: zh_CN.lang,
+      themeConfig: zh_CN.themeConfig,
+      description: zh_CN.description
+    },
+    zh_TW: {
+      label: '繁体中文',
+      lang: zh_TW.lang,
+      themeConfig: zh_TW.themeConfig,
+      description: zh_TW.description
+    },
+    en: {
+      label: 'English',
+      lang: en.lang,
+      themeConfig: en.themeConfig,
+      description: en.description
     }
+  }
 })
--- .vitepress/locales/zh_TW.ts
+++ .vitepress/locales/zh_TW.ts
@@ -1,33 +1,33 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-    lang: 'zh-TW',
-    description: '幹掉所有無良廣告',
+  lang: 'zh-TW',
+  description: '幹掉所有無良廣告',
 
-    themeConfig: {
-        nav: nav(),
+  themeConfig: {
+    nav: nav(),
 
-        lastUpdatedText: "最後更新",
-        darkModeSwitchLabel: '深色模式',
-        returnToTopLabel: '回到頂部',
-        outline: {
-            label: '目錄'
-        },
+    lastUpdatedText: '最後更新',
+    darkModeSwitchLabel: '深色模式',
+    returnToTopLabel: '回到頂部',
+    outline: {
+      label: '目錄'
+    },
 
-        editLink: {
-            pattern: 'https://github.com/TG-Twilight/AWAvenueAdsWebSite/edit/main/:path',
-            text: '在 GitHub 中編輯此頁'
-        } 
+    editLink: {
+      pattern: 'https://github.com/TG-Twilight/AWAvenueAdsWebSite/edit/main/:path',
+      text: '在 GitHub 中編輯此頁'
     }
+  }
 })
 
 function nav() {
-    return [
-        { text: '贊助我們', link: '/zh_TW/Donate.html' },
-        { text: '訂閱規則', link: '/zh_TW/Sub.html' },
-        { text: '感謝名單', link: '/zh_TW/Thank.html' },
-        { text: '用戶交流', link: '/zh_TW/Support.html' },
-        { text: '用戶協議', link: '/zh_TW/Protocol.html' },
-        { text: '使用教程', link: '/zh_TW/Knowledge.html' },
-    ]
+  return [
+    { text: '贊助我們', link: '/zh_TW/Donate.html' },
+    { text: '訂閱規則', link: '/zh_TW/Sub.html' },
+    { text: '感謝名單', link: '/zh_TW/Thank.html' },
+    { text: '用戶交流', link: '/zh_TW/Support.html' },
+    { text: '用戶協議', link: '/zh_TW/Protocol.html' },
+    { text: '使用教程', link: '/zh_TW/Knowledge.html' }
+  ]
 }
